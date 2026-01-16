2f7b7feebf758f7eb4c4e0e6a5495a3239247efc
Ilam Ball <informasiilam@gmail.com>
2026-01-17 04:03:26 +0700

    Add checkout functionality with CheckoutActivity, CheckoutViewModel, and related data models

---
+++ app/src/main/java/com/ampliguitar/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.ampliguitar.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BritishRed = Color(0xFFC8102E)
+val BritishBlue = Color(0xFF012169)
+val White = Color(0xFFFFFFFF)
